5f267a085bf121ff5567cff314b8bf02db9784f7
Christy Lazar <christylazarac@123.com>
2023-06-03 22:10:30 +0200

    Formatted code

--- src/DataFeeder.py
+++ src/DataFeeder.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sqlite3
 
+
 def read_file(file_path):
     return pd.read_excel(file_path)
 
@@ -9,20 +10,27 @@ def connect_db(path):
     connect = sqlite3.connect(path)
     return connect.cursor()
 
-def create_table(cursor,create_table_command):
+
+def create_table(cursor, create_table_command):
     cursor.execute(create_table_command)
 
-def insert_data(cur,  insert_command,insert_data,):
+
+def insert_data(
+    cur,
+    insert_command,
+    insert_data,
+):
     cur.execute(insert_command, insert_data)
-    
+
+
 # Load and read excel file
-df = read_file(r'data/source/export_patient.xlsx')
+df = read_file(r"data/source/export_patient.xlsx")
 
 # DB connection
 cur = connect_db("data/drwh.db")
 
 # Table creation
-sql_command = '''
+sql_command = """
 CREATE TABLE IF NOT EXISTS DWH_PATIENT
 (
 PATIENT_NUM INTEGER PRIMARY KEY,
@@ -47,17 +55,30 @@ BIRTH_LATITUDE FLOAT(126),
 BIRTH_LONGITUDE FLOAT(126),
 UPLOAD_ID INTEGER
 )
-'''
+"""
 create_table(cur, sql_command)
 
 
 # Feed database
-colomns =["NOM","PRENOM","DATE_NAISSANCE","SEXE","NOM_JEUNE_FILLE","HOSPITAL_PATIENT_ID","ADRESSE","TEL","CP","VILLE","PAYS","DATE_MORT"]
+colomns = [
+    "NOM",
+    "PRENOM",
+    "DATE_NAISSANCE",
+    "SEXE",
+    "NOM_JEUNE_FILLE",
+    "HOSPITAL_PATIENT_ID",
+    "ADRESSE",
+    "TEL",
+    "CP",
+    "VILLE",
+    "PAYS",
+    "DATE_MORT",
+]
 for index, row in df.iterrows():
     patient_num = row["HOSPITAL_PATIENT_ID"]
     last_name = row["NOM"]
     first_name = row["PRENOM"]
-    birth_date  = row["DATE_NAISSANCE"]
+    birth_date = row["DATE_NAISSANCE"]
     sex = row["SEXE"]
     maiden_name = row["NOM_JEUNE_FILLE"]
     residence_address = row["ADRESSE"]
@@ -68,11 +89,28 @@ for index, row in df.iterrows():
     date_mort = row["DATE_MORT"]
 
     sql_command = "INSERT INTO DWH_PATIENT (PATIENT_NUM, LASTNAME, FIRSTNAME, BIRTH_DATE, SEX, MAIDEN_NAME ,RESIDENCE_ADDRESS, PHONE_NUMBER, ZIP_CODE, RESIDENCE_CITY, DEATH_DATE, RESIDENCE_COUNTRY) VALUES ( ?, ?, ?,?,?, ?, ?, ? ,?, ?, ?, ? )"
-    insert_data(cur,sql_command,(patient_num, last_name,first_name, birth_date, sex, maiden_name, residence_address, phone_number, zip_code, residence_city, country, date_mort))
+    insert_data(
+        cur,
+        sql_command,
+        (
+            patient_num,
+            last_name,
+            first_name,
+            birth_date,
+            sex,
+            maiden_name,
+            residence_address,
+            phone_number,
+            zip_code,
+            residence_city,
+            country,
+            date_mort,
+        ),
+    )
 
 
 # second Table creation
-sql_command = '''
+sql_command = """
 CREATE TABLE IF NOT EXISTS DWH_PATIENT_IPPHIST
 (
 PATIENT_NUM INTEGER,
@@ -81,20 +119,14 @@ ORIGIN_PATIENT_ID VARCHAR2(40),
 MASTER_PATIENT_ID INTEGER,
 UPLOAD_ID INTEGER
 )
-'''
+"""
 create_table(cur, sql_command)
 
-#feed data into second table
+# feed data into second table
 for index, row in df.iterrows():
 
     patient_num = row["HOSPITAL_PATIENT_ID"]
     hospital_patient_id = row["HOSPITAL_PATIENT_ID"]
 
-
-
     sql_command = "INSERT INTO DWH_PATIENT_IPPHIST (PATIENT_NUM, HOSPITAL_PATIENT_ID) VALUES ( ?, ? )"
-    insert_data(cur,sql_command,(patient_num, hospital_patient_id))
-
-
-
-
+    insert_data(cur, sql_command, (patient_num, hospital_patient_id))
